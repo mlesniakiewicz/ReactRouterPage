30fabaa26be5f8e0021236f0a22fac9d6f53b944
Michal L <michal.lesniakiewicz@gmail.com>
2019-10-02 14:37:08 +0200

    Updates

---
+++ precache-manifest.41778e2d21a37336e6252d2104fdbfa8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "946cecf4603974bd227047e6ec2726a0",
+    "url": "/ReactRouterPage/static/media/header4.946cecf4.jpg"
+  },
+  {
+    "revision": "a394b260aa0d905a7fafcc49f1d30b9b",
+    "url": "/ReactRouterPage/static/media/header3.a394b260.jpg"
+  },
+  {
+    "revision": "d13f6ea6502b318a0c977747659e8d45",
+    "url": "/ReactRouterPage/static/media/header2.d13f6ea6.jpg"
+  },
+  {
+    "revision": "fd4c82549974e837c43d0894c81379bc",
+    "url": "/ReactRouterPage/static/media/header1.fd4c8254.jpg"
+  },
+  {
+    "revision": "b6965771fbac4d6b6848",
+    "url": "/ReactRouterPage/static/js/runtime~main.b6965771.js"
+  },
+  {
+    "revision": "d8f5a0998b748072604d",
+    "url": "/ReactRouterPage/static/js/main.d8f5a099.chunk.js"
+  },
+  {
+    "revision": "234a5405d759fb67def8",
+    "url": "/ReactRouterPage/static/js/2.234a5405.chunk.js"
+  },
+  {
+    "revision": "d8f5a0998b748072604d",
+    "url": "/ReactRouterPage/static/css/main.ca03c6e8.chunk.css"
+  },
+  {
+    "revision": "b6cda098a97914e73e21ac7f4d181826",
+    "url": "/ReactRouterPage/index.html"
+  }
+];
